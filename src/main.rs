d63ce565b64a286e2b529c9616f01683c6a0f2af
thomvil <thomvil87@gmail.com>
2021-12-13 09:03:44 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+// #![allow(dead_code, unused_imports, unused_variables)]
+#![warn(
+    anonymous_parameters,
+    missing_copy_implementations,
+    missing_debug_implementations,
+    nonstandard_style,
+    rust_2018_idioms,
+    single_use_lifetimes,
+    trivial_casts,
+    trivial_numeric_casts,
+    unreachable_pub,
+    unused_extern_crates,
+    unused_qualifications,
+    variant_size_differences
+)]
+#![forbid(unsafe_code)]
+
+fn main() {}
